6362174d6961883714e2948230e7a392819e8f59
kaan koc <kaankoc01@gmail.com>
2025-04-17 04:58:00 +0300

    category , course feature ve baseEntity oluşturuldu

---
+++ UdemyMicroservice.Catalog.API/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using UdemyMicroservice.Catalog.API.Features.Courses;
+using UdemyMicroservice.Catalog.API.Repositories;
+
+namespace UdemyMicroservice.Catalog.API.Features.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = default;
+        public List<Course> Courses { get; set; }
+    }
+}
---
+++ UdemyMicroservice.Catalog.API/Repositories/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+namespace UdemyMicroservice.Catalog.API.Repositories
+{
+    public class BaseEntity
+    {
+        // snow flakes
+        [BsonElement("_id")]
+        public Guid Id { get; set; }
+    }
+}
